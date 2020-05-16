6345b3f39ffcb99f3e9998199eba1e233b96c5ca
TruongDucVu <18520194@gm.uit.edu.vn>
2020-05-16 17:08:34 +0700

    project

--- Folio/knn.py
+++ Folio/knn.py
@@ -3,25 +3,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from pyimagesearch.preprocessing import SimplePreprocessor
-from pyimagesearch.datasets import SimpleDatasetLoader
+from DataLoader.simpledatasetloader import SimpleDatasetLoader
+from preprocessing.simplepreprocessor import SimplePreprocessor
 from imutils import paths
-import argparse
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-help="path to input dataset")
-ap.add_argument("-k", "--neighbors", type=int, default=1,
-help="# of nearest neighbors for classification")
-ap.add_argument("-j", "--jobs", type=int, default=-1,
-help="# of jobs for k-NN distance (-1 uses all available cores)")
-args = vars(ap.parse_args())
 
 # grab the list of images that we’ll be describing
 print("[INFO] loading images...")
-imagePaths = list(paths.list_images(args["dataset"]))
-
+imagePaths = list(paths.list_images('Dataset'))
+print(imagePaths)
+"""
 # initialize the image preprocessor, load the dataset from disk,
 # and reshape the data matrix
 sp = SimplePreprocessor(32, 32)
@@ -44,8 +35,9 @@ labels = le.fit_transform(labels)
 
 # train and evaluate a k-NN classifier on the raw pixel intensities
 print("[INFO] evaluating k-NN classifier...")
-model = KNeighborsClassifier(n_neighbors=args["neighbors"],
-    n_jobs=args["jobs"])
+model = KNeighborsClassifier(n_neighbors=1,
+    n_jobs=-1)
 model.fit(trainX, trainY)
 print(classification_report(testY, model.predict(testX),
     target_names=le.classes_))
+"""
