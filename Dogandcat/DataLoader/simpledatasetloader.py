c16c7a141ef5a02b2cbd082ea6054a974a7b7da5
CS114.K21.KHTN <62535444+ducvuuit@users.noreply.github.com>
2020-05-19 23:43:23 +0700

    Update simpledatasetloader.py

--- Dogandcat/DataLoader/simpledatasetloader.py
+++ Dogandcat/DataLoader/simpledatasetloader.py
@@ -24,7 +24,7 @@ class SimpleDatasetLoader:
             # that our path has the following format:
             # /path/to/dataset/{class}/{image}.jpg
             image = cv2.imread(imagePath)
-            label = imagePath.split(os.path.sep)[-2]
+            labels = imagePath.split(os.path.sep)[-2]
 
             # check to see if our preprocessors are not None
             if self.preprocessors is not None:
@@ -36,7 +36,7 @@ class SimpleDatasetLoader:
             # treat our processed image as a "feature vector" by
             # updating the data list followed by the label
             data.append(image)
-            label.append(label)
+            labels.append(label)
             # show an update every ‘verbose‘ images
             if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
                 print("[INFO] processed {}/{}".format(i + 1, len(imagePaths)))
