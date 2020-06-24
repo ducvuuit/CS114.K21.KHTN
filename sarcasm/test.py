7db6163ff84c530a52d98c9d284c427b35324d3a
ducvuuit <18520194@gm.uit.edu.vn>
2020-06-24 08:39:24 +0700

    fix

--- sarcasm/test.py
+++ sarcasm/test.py
@@ -1,3 +1,4 @@
+# Theem cacs thuw vien can thiet
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
@@ -8,6 +9,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 
+# tai len data
 import pandas as pd
 
 data = pd.read_json("Dataset/Sarcasm_Headlines_Dataset_v2.json",lines=True)
@@ -17,15 +19,15 @@ Y = data['is_sarcastic']
 trainX, testX, trainY, testY = train_test_split(X, Y, test_size=0.25, random_state=42)
 
 print("[INFO] Used CounterVectorizer ... ")
-# initialize CountVectorizer
+# su dung phuong phap vector hoa van ban
 cv = CountVectorizer()
 
-# transform words to vetor
+# chuyen doi tu thanh vecor
 trainX = cv.fit_transform(trainX.values).toarray()
 trainVocab = cv.vocabulary_
 cv = CountVectorizer(vocabulary=trainVocab)
 testX = cv.fit_transform(testX.values).toarray()
-
+'''
 for i in range(3):
 
     if i == 0:
@@ -42,3 +44,11 @@ for i in range(3):
     model.fit(trainX, trainY)
     predictions = model.predict(testX)
     print(classification_report(testY, predictions))
+    print("\n---------------------------------------\n")
+'''
+from sklearn.naive_bayes import GaussianNB
+model = GaussianNB()
+print("   [INFO] evaluating GaussianNB...")
+model.fit(trainX, trainY)
+predictions = model.predict(testX)
+print(classification_report(testY, predictions))
