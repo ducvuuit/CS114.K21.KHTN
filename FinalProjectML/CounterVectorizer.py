7545e90b61f70e91552b744a4c5c5180a2260ce9
CS114.K21.KHTN <62535444+ducvuuit@users.noreply.github.com>
2020-07-01 13:04:53 +0700

    Create CounterVectorizer.py
    up

---
+++ FinalProjectML/CounterVectorizer.py
@@ -0,0 +1,10 @@
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+
+import pandas as pd 
